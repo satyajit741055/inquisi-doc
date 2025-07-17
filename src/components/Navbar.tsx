9c30d232911c90760ce0fd6dc28bb22d2fa44a98
satyajit741055 <satyajit7410@gmail.com>
2025-07-17 19:55:48 +0530

    Appwrite Authentication services

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+import { ArrowRight } from 'lucide-react'
+
+const Navbar = () => {
+
+    return (
+        <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+            <MaxWidthWrapper>
+                <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+                    <Link
+                        href='/'
+                        className='flex z-40 font-semibold'>
+                        <span>INQUISI-DOC</span>
+                    </Link>
+
+                    {/* Add mobile Navbar */}
+                    <div className='hidden items-center space-x-4 sm:flex'>
+                        <>
+                            <Link className={buttonVariants({
+                                variant: "ghost",
+                                size: 'sm'
+                            })}
+                                href="/pricing"> Pricing
+                            </Link>
+                        </>
+                    </div>
+                </div>
+            </MaxWidthWrapper>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/conf/conf.ts
@@ -0,0 +1,6 @@
+const conf = {
+    appwriteUrl: String(process.env.APPWRITE_URL),
+    appwriteProjectId: String(process.env.APPWRITE_PROJECT_ID),
+}
+
+export default conf
